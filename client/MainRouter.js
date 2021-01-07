fc941ebe0b6b7491a99ea811ee4da4e180c1598e
Chidera Ogu <chidera@Chideras-MacBook-Pro.local>
2021-01-07 16:54:09 +0100

    adding mern skeleton files

---
+++ client/MainRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Switch} from 'react-router-dom';
+import Home from './core/Home';
+import Signup from './user/Signup';
+import Signin from './auth/Signin';
+import Users from './user/Users';
+import Profile from './user/Profile';
+import PrivateRoute from './auth/privateRoute';
+import EditProfile from './user/EditProfile';
+
+const MainRouter = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/users" component={Users} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/signin" component={Signin} />
+                <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+                <Route path="/user/:userId" component={Profile}/>
+            </Switch>
+        </div>
+    )
+}
+
+export default MainRouter;
